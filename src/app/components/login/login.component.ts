287256cbb39fb86bef48deb69966f35a471125ea
leidy <leidy-ktpm@hotmail.com>
2022-05-06 15:35:35 -0500

    agregue el componente y metodo login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, Injectable} from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Injectable()
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent{
+  user= {
+    email:'',
+    password:''
+  }
+  constructor(
+              private authService: AuthService,
+              private router: Router  ){}
+  logger(){
+    console.log(this.user);
+    const {email, password}= this.user;
+    this.authService.login(email,password).then(res =>{
+      this.router.navigate(['/bienvenido']);
+      console.log('Usuario loggeado', res);
+    }
+    )
+  }
+  }
+
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,6 +8,8 @@ import { Component} from '@angular/core';
 })
 export class WelcomeComponent {
 
-  constructor(){}
-
+  constructor(
+              private router: Router
+  ){
+  }
 }
