09ba3680e3643ed248937a17027932d396bb1bb0
leidy <leidy-ktpm@hotmail.com>
2022-05-17 10:34:08 -0500

    creé guard/rol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AuthGuard } from './guard/auth.guard';
+import { map } from 'rxjs/operators'
+
+
+const rolAdmin = 'SGtWIZxE4KOMqXpRJgCHSItLbyK2';
+const onlyAdmin= () => map((user:any) => !!user && user.uid === rolAdmin )
 
 const routes: Routes = [
   {path: '', component: BeginComponent },
